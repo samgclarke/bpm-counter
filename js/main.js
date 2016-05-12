9e98b455124f66e71e50534602f5491d0a7cd8b2
Sam Clarke <samclarke.g@gmail.com>
2016-05-12 16:25:04 -0700

    styling updates, genre service

--- js/main.js
+++ js/main.js
@@ -2,6 +2,18 @@ var tally = [];
 var timing = {start: undefined, end: undefined};
 document.addEventListener("keydown", keyDowner, false);
 
+var genres = {
+  80: 'Reggae',
+  90: 'Hip Hop',
+  100: 'House',
+  110: 'House',
+  120: 'Breaks',
+  130: 'Breaks',
+  140: 'Trance',
+  160: 'Drum & Bass',
+  170: 'Drum & Bass'
+}
+
 function reset(elementId) {
   console.log('reset', elementId);
   tally = [];
@@ -13,17 +25,19 @@ function reset(elementId) {
 };
 
 function keyDowner(e) {
-  var bpm, avg, sum = 0;
+  var timestamp, bpm, avg, sum = 0;
+
+  timestamp = (e) ? e.timeStamp : new Date();
 
   if (!timing.start) {
-    timing.start = e.timeStamp;
+    timing.start = timestamp;
   } 
   else if (!timing.end) {
-    timing.end = e.timeStamp
+    timing.end = timestamp;
   }
   else if (timing.start && timing.end) {
     timing.start = timing.end;
-    timing.end = e.timeStamp;
+    timing.end = timestamp;
   }
   // calculate bpm
   bpm = 60000 / (timing.end - timing.start);
@@ -38,7 +52,13 @@ function keyDowner(e) {
     // average all bpms
     avg = Math.round(sum / tally.length);
     if (avg) {
-      document.getElementById("output").innerHTML = avg;
+      document.getElementById("output").innerHTML = avg + ' bpm';
+
+      // get genre
+      var round = Math.floor(avg / 10) * 10;
+      var genre = genres[round];
+      console.log('round, genre', [round, genre]);
+      document.getElementById("genre").innerHTML = genre;
     };
   }
 };
